0257f37a89bceaae6cd0b728f5191211dffceff9
Abhi Tyagi <tyagiabhi9548@gmail.com>
2026-05-28 22:31:20 +0530

    Performed the CRUD operations using REST Api's

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const { v4 : uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
+
 uuidv4(); 
 
 const path = require('path');
 
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
+app.use(methodOverride('_method'));	
 
 app.set('view engine' , 'ejs');
 app.set("views" , path.join(__dirname , "views"));
@@ -15,12 +18,15 @@ app.use(express.static(path.join(__dirname , "public")));
 let port = 8080 ;
 
 let posts = [{
-	id : uuidv4(),
-	username : "Abhi Tyagi",
-	content : "This is my first post"
-}
-
-	
+		id : uuidv4(),
+		username : "Abhi Tyagi",
+		content : "This is my first post"
+	},
+	{
+		id : uuidv4(),
+		username : "haka",
+		content : "I am also posting here..."
+	}		
 ]
 
 app.get('/', (req, res) => {
@@ -49,18 +55,25 @@ app.get('/posts/:id', (req, res) => {
 	res.render("show.ejs", { post : post});
 });
 
-app.post('/posts/:id',(req,res) =>{
+app.patch('/posts/:id',(req,res) =>{
 	let { id } = req.params;
 	let post = posts.find((p) => id === p.id);
 	let newContent = req.body.content ;
 	post.content = newContent ;
-	req.redirect('/posts');
+	console.log(post);
+	res.redirect('/posts');
 });
 
 app.get('/posts/:id/edit', (req, res) =>{
 	let {id} = req.params ;
 	let post = posts.find((p) => id === p.id);
-	res.render("edit.ejs");
+	res.render("edit.ejs", {post});
+});
+
+app.delete('/posts/:id', (req, res) => {
+	let { id } = req.params ;
+	posts = posts.filter((p) => id !== p.id) ;
+	res.redirect('/posts');
 });
 
 app.listen(port , () => {
